aefb2ec795c17367aaa995447c8e8d2e3f6424f8
SUHO PARK <duglikespiano@gmail.com>
2026-04-22 17:46:15 +0900

    37 - LIMIT AREA

---
+++ 37_limit_area/app.js
@@ -0,0 +1,19 @@
+const cells = document.querySelectorAll('.grid__cell');
+const marker = document.querySelector('.grid__marker');
+
+window.addEventListener('mousemove', (e) => {
+	marker.style.top = `${e.clientY - marker.clientWidth / 2}px`;
+	marker.style.left = `${e.clientX - marker.clientHeight / 2}px`;
+
+	if (marker.getBoundingClientRect().right > window.innerWidth) {
+		marker.style.left = `${window.innerWidth - marker.clientWidth}px`;
+	} else if (marker.getBoundingClientRect().left < 0) {
+		marker.style.left = `0px`;
+	}
+
+	if (marker.getBoundingClientRect().top < 0) {
+		marker.style.top = `0px`;
+	} else if (marker.getBoundingClientRect().bottom > window.innerHeight) {
+		marker.style.top = `${window.innerHeight - marker.clientHeight}px`;
+	}
+});
